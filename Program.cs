ed8425e7b0f2ac49d2a50f4b0b48a66d4a8d878f
Taras Byalyk <byalykt@hotmail.com>
2026-02-04 10:42:06 +0100

    work on routing, DI

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+builder.Services.AddScoped<NetworkClient>();
+builder.Services.AddSingleton<MessageFactory>();
+builder.Services.AddSingleton(provider => new EmailServerSettings(Host: "smtp.gmail.com", Port: 25));
+
 builder.Services.AddDbContext<MyAppContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -30,8 +35,11 @@ app.MapGet("/teapot", (HttpResponse res) =>
     }
 );
 
+LinkGenerator links = app.Services.GetRequiredService<LinkGenerator>();
 
-app.MapGet("/cart", (MyAppContext myAppContext) => myAppContext.Cart);
+app.MapGet("/product/{id}", (int id) => { Results.Ok(new { Source = "route", Value = id }); });
+
+app.MapGet("/product", (int id) => { return $"Received {id} in route query"; });
 
 app.MapGet("/cart/{id}", (long id, MyAppContext myAppContext) =>
 {
@@ -57,4 +65,62 @@ app.MapPost("/cart", ([FromBody] Cart cart, MyAppContext myAppContext) =>
     myAppContext.Cart.Add(cart);
     myAppContext.SaveChanges();
 });
+
+app.MapGet("register/{username}", RegisterUser);
+
 app.Run();
+
+string RegisterUser(string username, IEmailSender emailSender)
+{
+    emailSender.SendEmail(username);
+    return $"Email sent to {username}";
+}
+
+public record EmailServerSettings(string Host, int Port);
+
+public class NetworkClient
+{
+    private readonly EmailServerSettings _settings;
+
+    public NetworkClient(EmailServerSettings settings)
+    {
+        _settings = settings;
+    }
+
+    public void SendEmail(string email)
+    {
+        Console.WriteLine($"Hello, {email}! This is a test!");
+    }
+}
+
+public class MessageFactory
+{
+    public string Create(string username)
+    {
+        return $"{username}@gmail.com";
+    }
+}
+
+public interface IEmailSender
+{
+    public void SendEmail(string username);
+}
+
+public class EmailSender : IEmailSender
+{
+    private readonly NetworkClient _client;
+    private readonly MessageFactory _factory;
+
+    public EmailSender(MessageFactory factory, NetworkClient client)
+    {
+        _factory = factory;
+        _client = client;
+    }
+
+    public void SendEmail(string username)
+    {
+        var email = _factory.Create(username);
+        _client.SendEmail(email);
+        Console.WriteLine($"Email sent to {username}");
+    }
+}
